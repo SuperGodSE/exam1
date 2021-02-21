4a98d83e2b06490a999f817ab90cea6242656ea4
Student User <student@workstation.lab.example.com>
2021-02-21 11:40:40 -0500

    updated the probes

--- node1/app.js
+++ node1/app.js
@@ -5,9 +5,15 @@ app.get('/', function (req, res) {
   res.send('Hello World! this is the first edit\n');
 });
 
-app.get('/fakyou'. function(req, res){
+app.get('/ready1'. function(req, res){
   req.send('this is a readiness probe - fakkkkkkkkk you\n');
 });
+
+
+app.get('/healthy1'. function(req, res){
+  req.send('this is a healthy probe - I am healthy hahahahahahahahahaha \n');
+});
+
 
 
 app.listen(8080, function () {

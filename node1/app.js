0b1e139b8c170a61f3933aeb5b02bbaa27800dea
Student User <student@workstation.lab.example.com>
2021-02-21 11:27:36 -0500

    added readiness endpoint

--- node1/app.js
+++ node1/app.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World! this is the first edit\n');
 });
 
+app.get('/fakyou'. function(req, res){
+  req.send('this is a readiness probe - fakkkkkkkkk you\n');
+});
+
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });

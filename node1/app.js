50dcf808c348f32d31a346d9419c2a3cf466ae54
Student User <student@workstation.lab.example.com>
2021-02-20 20:58:38 -0500

    first update

--- node1/app.js
+++ node1/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World! this is the first edit\n');
 });
 
 app.listen(8080, function () {
